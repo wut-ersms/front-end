e40425892307568b8c775ce1bf52edb0687c0d29
patrykploskipw <01168908@pw.edu.pl>
2025-05-19 21:05:28 +0200

    pedro

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 ### Setup
 1. ```docker build -t ersms-frontend .```
 
-2. ```docker run .```
+2. ```docker run -p 5000:5000 -it ersms-frontend```
--- app/views/main.py
+++ app/views/main.py
@@ -26,30 +26,14 @@ table_status = {
 }
 
 
-@bp.route("/map", methods=["GET"])
-def map():
-    return render_template("main/mapa.html", table_status=table_status)
+@bp.route("/", methods=["POST", "GET"])
+def index():
+    return render_template("pedro.html")
 
 
-@bp.route("/sector1")
-def sector1():
-    sector1_status = {
-        table: info for table, info in table_status.items() if info["sector"] == 1
-    }
-    return render_template("main/sector1.html", table_status=sector1_status)
-
-
-@bp.route("/sector2")
-def sector2():
-    sector2_status = {
-        table: info for table, info in table_status.items() if info["sector"] == 2
-    }
-    return render_template("main/sector2.html", table_status=sector2_status)
-
-
-@bp.route("/get_table_status", methods=["GET"])
-def get_table_status():
-    return jsonify(table_status)
+@bp.route("/pedro")
+def pedro():
+    return render_template("main/pedro.html")
 
 
 @bp.route("/main")
@@ -57,49 +41,70 @@ def main():
     return render_template("main/main.html")
 
 
-@bp.route("/update_tables", methods=["POST"])
-def update_tables():
-    data = request.json
-    logging.debug("Received data: %s", data)  # Logowanie przy użyciu modułu logging
-
-    if not data or "table_number" not in data or "status" not in data:
-        logging.error("Invalid data provided")
-        return jsonify({"error": "Invalid data provided"}), 400
-
-    table_number = data.get("table_number")
-    status = data.get("status")
-
-    if table_number not in table_status:
-        logging.error("Invalid table number")
-        return jsonify({"error": "Invalid table number"}), 400
-
-    table_status[table_number]["status"] = status
-    logging.debug("Table status updated: %s", table_status)
-    # Aktualizacja mapy na podstawie stanu stolików
-    sector = table_status[table_number]["sector"]
-    free_tables = [
-        table_number
-        for table_number, info in table_status.items()
-        if info["status"] == "free" and info["sector"] == sector
-    ]
-    image_file = choose_sector_image(free_tables, sector)
-    logging.debug(
-        "Image file: %s", "/static/images/maps/" + image_file
-    )  # Logowanie URL obrazu
-
-    socketio.emit(
-        f"update_map_s{sector}", {"image_file": "/static/images/maps/" + image_file}
-    )
-
-    free_tables_per_sector = get_free_tables_per_sector(table_status)
-    image_file = choose_total_image(free_tables_per_sector)
-    logging.debug(
-        "Total image file: %s", "/static/images/maps/" + image_file
-    )  # Logowanie URL obrazu
-    socketio.emit("update_map", {"image_file": "/static/images/maps/" + image_file})
-    return jsonify({"success": True}), 200
+# @bp.route("/map", methods=["GET", "POST"])
+# def map():
+#     return render_template("main/mapa.html", table_status=table_status)
 
 
-@bp.route("/pedro")
-def pedro():
-    return render_template("main/pedro.html")
+# @bp.route("/sector1")
+# def sector1():
+#     sector1_status = {
+#         table: info for table, info in table_status.items() if info["sector"] == 1
+#     }
+#     return render_template("main/sector1.html", table_status=sector1_status)
+
+
+# @bp.route("/sector2")
+# def sector2():
+#     sector2_status = {
+#         table: info for table, info in table_status.items() if info["sector"] == 2
+#     }
+#     return render_template("main/sector2.html", table_status=sector2_status)
+
+
+# @bp.route("/get_table_status", methods=["GET"])
+# def get_table_status():
+#     return jsonify(table_status)
+
+
+# @bp.route("/update_tables", methods=["POST"])
+# def update_tables():
+#     data = request.json
+#     logging.debug("Received data: %s", data)  # Logowanie przy użyciu modułu logging
+
+#     if not data or "table_number" not in data or "status" not in data:
+#         logging.error("Invalid data provided")
+#         return jsonify({"error": "Invalid data provided"}), 400
+
+#     table_number = data.get("table_number")
+#     status = data.get("status")
+
+#     if table_number not in table_status:
+#         logging.error("Invalid table number")
+#         return jsonify({"error": "Invalid table number"}), 400
+
+#     table_status[table_number]["status"] = status
+#     logging.debug("Table status updated: %s", table_status)
+#     # Aktualizacja mapy na podstawie stanu stolików
+#     sector = table_status[table_number]["sector"]
+#     free_tables = [
+#         table_number
+#         for table_number, info in table_status.items()
+#         if info["status"] == "free" and info["sector"] == sector
+#     ]
+#     image_file = choose_sector_image(free_tables, sector)
+#     logging.debug(
+#         "Image file: %s", "/static/images/maps/" + image_file
+#     )  # Logowanie URL obrazu
+
+#     socketio.emit(
+#         f"update_map_s{sector}", {"image_file": "/static/images/maps/" + image_file}
+#     )
+
+#     free_tables_per_sector = get_free_tables_per_sector(table_status)
+#     image_file = choose_total_image(free_tables_per_sector)
+#     logging.debug(
+#         "Total image file: %s", "/static/images/maps/" + image_file
+#     )  # Logowanie URL obrazu
+#     socketio.emit("update_map", {"image_file": "/static/images/maps/" + image_file})
+#     return jsonify({"success": True}), 200
