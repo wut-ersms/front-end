00e073ac6c78670b0d0bf2bb3630b94c4fbc469e
patrykploskipw <01168908@pw.edu.pl>
2025-05-20 22:13:30 +0200

    context processor

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, session
 from flask_socketio import SocketIO
 
 # from .config import Config
@@ -9,14 +9,23 @@ socketio = SocketIO()
 def create_app():
     app = Flask(__name__)
     # app.config.from_object(Config())
+    app.secret_key = "twoj_super_tajny_klucz"
 
     socketio.init_app(app)
 
-    from .views import main, transactions_history, order, desktop
+    from .views import main, transactions_history, order, desktop, auth
 
     app.register_blueprint(main.bp)
     app.register_blueprint(transactions_history.bp)
     app.register_blueprint(order.bp)
     app.register_blueprint(desktop.bp)
+    app.register_blueprint(auth.bp)
+
+    @app.context_processor
+    def inject_user():
+        return {
+            "user_logged_in": session.get("logged_in", False),
+            "username": session.get("username", None),
+        }
 
     return app
