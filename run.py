c95dfe590cdfbface08a5c72b56dcc67cce6f20d
patrykploskipw <01168908@pw.edu.pl>
2025-05-26 18:15:48 +0200

    added default parameters

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ load_dotenv()
 app = create_app()
 
 if __name__ == "__main__":
-    socketio.run(app, host=os.getenv("FLASK_RUN_HOST") , port=os.getenv("FLASK_RUN_PORT") , debug=os.getenv("DEBUG"))
+    socketio.run(app, host=os.getenv("FLASK_RUN_HOST", "0.0.0.0") , port=os.getenv("FLASK_RUN_PORT", "5000") , debug=os.getenv("DEBUG", False))
